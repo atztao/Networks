1ad496c0563ecd391e2904ae7fde9ca7bd0f05da
ztao1991 <ztao1991@gmail.com>
2016-12-09 18:41:30 +0800

    Add Exercise 3 Homework

---
+++ Exercise/Net_ex3/P_t.py
@@ -0,0 +1,18 @@
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def Net(P, t):
+    a = 0.5
+    return -a * P
+
+t = np.linspace(0.0, 100, 100)
+
+y = odeint(Net, 1, t)
+
+plt.plot(t, y)
+plt.xlabel('t')
+plt.ylabel('P(t)')
+plt.savefig("P_t.png")
+plt.show()
